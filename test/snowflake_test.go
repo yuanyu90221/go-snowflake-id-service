99ab7eba79653f977e2c7cb0b01b70867c983153
gsonLiang <yuanyu.liang@tkspring.com>
2023-01-16 13:34:26 +0800

    update benchmark

--- test/snowflake_test.go
+++ test/snowflake_test.go
@@ -36,29 +36,13 @@ func Test_NextID(t *testing.T) {
 }
 
 func BenchmarkTest(b *testing.B) {
-	// create checkMap for check duplicated
-	checkMap := make(map[int64]struct{})
 	epoch := time.Now().UTC().UnixMilli() - 300000
 	shardTransactionId := 0
 	snowflakeTx, err := snowflake.NewShard(int64(shardTransactionId), epoch)
 	if err != nil {
 		b.Error(err)
 	}
-	// buffer equals 10 means time concurrent users
-	idChannel := make(chan int64, 10)
 	for count := 1; count <= b.N; count++ {
-		go func(ch chan int64) {
-			// get snowflakeTx and push to ch
-			ch <- snowflakeTx.NextID()
-		}(idChannel)
-	}
-	for count := 1; count <= b.N; count++ {
-		// receive from idChannel
-		id := <-idChannel
-		if _, isExist := checkMap[id]; isExist {
-			b.Errorf("already exist id : %v", id)
-		} else {
-			checkMap[id] = struct{}{}
-		}
+		snowflakeTx.NextID()
 	}
 }
